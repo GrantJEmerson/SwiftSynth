64e273e0619e3b646a69c8e84434e1a7b40aabdf
Grant Emerson <grant@821apps.com>
2019-07-21 18:04:00 -0500

    Initial Commit

---
+++ Swift Synth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Synth
+//
+//  Created by Grant Emerson on 7/21/19.
+//  Copyright © 2019 Grant Emerson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
